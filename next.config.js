3ccc6d42becd3e845e2783cf366b20b84df0d58e
mbrio <moritz.e.braun@gmail.com>
2024-10-28 14:52:56 +0100

    CORS policy

--- next.config.js
+++ next.config.js
@@ -5,6 +5,27 @@ module.exports = {
     eslint: {
       ignoreDuringBuilds: true,
     },
+    async headers() {
+      return [
+        {
+          source: '/(.*)',
+          headers: [
+            {
+              key: 'Access-Control-Allow-Origin',
+              value: 'https://digit.cologne',
+            },
+            {
+              key: 'Access-Control-Allow-Methods',
+              value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+            },
+            {
+              key: 'Access-Control-Allow-Headers',
+              value: 'X-Requested-With, Content-Type, Authorization',
+            },
+          ],
+        },
+      ];
+    }
   }
   
 module.exports = nextConfig
